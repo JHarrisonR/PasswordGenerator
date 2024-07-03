eea50ce76d605106f3affd67af4a0e8bc16afcb1
JHarrisonR <john_HSR1@hotmail.com>
2024-07-03 10:50:36 -0300

    add attributs

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:flutter/services.dart';
 import 'package:password_generator/constants.dart';
-// import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
